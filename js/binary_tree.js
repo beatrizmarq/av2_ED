5681877c2c5eccb3e884107981681162e65deb79
Ana Beatriz Marques de L. Gomes <67196411+beatrizmarq@users.noreply.github.com>
2020-06-24 20:13:01 -0300

    Update binary_tree.js

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -1,10 +1,17 @@
 class BinaryTree {
-    // inicializa a raiz como nula
+    // inicia a raiz como nula   #comentado
     constructor() {
         this.root = null
     }
 
-    //exibe o menor valor da arvore
+    //mostra o valor menor da arvore
+       
+    /* a currente que é um variavel ira receber o valor da variavel root
+        se current for igual a null retornara -1 porque nao ira ter valor dentro ou também retorna null
+        caso não entra dentro de um laço 
+        enquanto o valor da esquerda for != de null current recebera current.left
+        quando o valor da esquerda for == a null encerra o while e current tera o valor menor  #comentado
+    */
     min() {
         let current = this.root
         if (current == null)
@@ -14,7 +21,15 @@ class BinaryTree {
         return current.content
     }
 
-    //exibe o maior valor da arvore
+    //mostra o maior valor da arvore
+       
+     /* 
+        A current variavel recebe o valor da varivel root
+        Se current for == a null ira retornar -1 porque nao tera valor dentro ou entao retornara null
+        caso inverta os papeis entra dentro do laço 
+        enquanto o valor da direita for != de null current recebe current.left
+        quando o valor da direita for == a null termina o while e current tem o valor maior
+    */
     max() {
         let current = this.root
         if (current == null)
@@ -25,10 +40,18 @@ class BinaryTree {
     }
 
     //insere o elemento da arvores
-    insert(element) {
+    insert(element) { //recebe uma referencia do no 
         this.root = this.insertNode(this.root, element)
+        //retorna uma referencia do no
     }
-
+    
+    //IMPLEMENTANDO O INSERT DA ARVORE
+    /*recebe uma referencia e insere o no
+      Ele analisa se é nulu e insere
+      analisa se é maior que a raiz
+      Se sim ele insere na direta
+      Se nao ele insere a esquerda  #comentado
+    */
     insertNode(rootNode, element) {
         if (rootNode == null)
             return new Node(element)
@@ -40,10 +63,16 @@ class BinaryTree {
     }
 
     //executa a função callback para cada nó, em ordem
+    //a funcao recebe callback
     inOrderTraverse(callback) {
         this.inOrderVisitor(this.root, callback)
     }
-
+    
+    /*analisa se o no e nulo
+     *se sim nao returna nada
+     *caso contrario manda mostrar em ordem
+     *esqueda,conteudo e direita do no    #comentado
+    */
     inOrderVisitor(node, callback) {
         if (node == null)
             return
@@ -56,7 +85,13 @@ class BinaryTree {
     preOrderTraverse(callback) {
         this.preOrderVisitor(this.root, callback)
     }
-
+    //recebe o no e o callback 
+    /*
+        se no for nullo nao retorne nada
+        se nao passe o conteudo do no dentro do callback
+        e depois exibe a esquerda do no
+        e logo em seguida a direita do no  #comentado
+    */ 
     preOrderVisitor(node, callback) {
         if (node == null)
             return
@@ -66,10 +101,17 @@ class BinaryTree {
     }
 
     //executa a função callback para cada nó, em pós-ordem
+    
     postOrderTraverse(callback) {
         this.postOrderVisitor(this.root, callback)
     }
-
+    //pos ordem recebe o metodo e a função
+       /* 
+        se o no for nullo nao retorne nada
+        se nao ira passar o conteudo do no dentro do callback
+        e depois exibir a esquerda do no
+        e logo em seguida a direita do no  #comentado
+       */
     postOrderVisitor(node, callback) {
         if (node == null)
             return
@@ -79,12 +121,12 @@ class BinaryTree {
     }
 
     //retorna true se o valor já existe na arvore 
-    //     Busca na árvore binária
-    //    1. É nulo? o elemento não existe
-    //    2. É igual ao conteúdo? achou
-    //    3. É maior que o conteúdo?
-    //       3.1 busca de direita
-    //       3.2 busca na esquerda
+    //  Busca na árvore binária
+    //  se é nulo, o elemento não existe
+    //  se é igual ao conteúdo, encontrou
+    //  se é maior que o conteúdo
+    //  busca na direita
+    //  busca na esquerda      #comentado
 
     search(value) {
         return this.searchVisitor(this.root, value)
@@ -102,23 +144,42 @@ class BinaryTree {
     }
 
     //remove um elemento existente na arvore o retorna
+       
+    //remove e retorna a arvore carregada
     remove(value) {
         this.root = this.removeVisitor(this.root, value)
     }
-
+    /* 
+        Se o valor do no for == ao valor
+        entra dentro do 2º if
+        Se o lado esquerdo for == ao lado direito retorna null
+        Se o lado direito for nulo retorna o lado esquerdo
+        Se o lado esquerdo for == a null retorna o lado direito
+        E se nao atender nenhum das condições passadas
+        O novo no recebera o valor do lado direito
+        O current também ira recebe o mesmo valor
+        E o valor do lado esquerdo for diferemte de null
+        Current recebera o valor do lado esquerdo
+        E retornara o novo no
+        Se o valor de content nao for igual a value
+        E value for > o valor do no entra dentro do if
+        E o valor da esquerda e removido
+        Caso contrario o valor da direita é tirado
+        E entao retornara o no                             #comentado
+    */
     removeVisitor(node, value) {
         if (node.content == value) {
             if (node.left == node.right) {
-                //nao tem filhos - Grau 0
+                //nao possui filhos - Grau 0
                 return null
             } else if (node.right == null) {
-                //não tem filhos na direita, e tem nó na esqueda - Grau 1
+                //nao possui filhos na direita, e tem nó na esqueda - º 1
                 return node.left
             } else if (node.left == null) {
-                //não tem filhos da esquerda, e tem nó da direita - Grau 1
+                //nao possui filhos da esquerda, e tem nó da direita - º 1
                 return node.right
             } else {
-                // tem os dois ramos - Grau 2
+                // tem os dois ramos - Grau º 2                   #comentado
                 const newRoot = node.right
                 let current = node.right;
                 while (current.left != null)
@@ -133,12 +194,16 @@ class BinaryTree {
         }
         return node;
     }
-
     //exibe a altura da arvore
     height() {
         return this.heightVisitor(this.root)
     }
-
+    /*recebe um no e sera nulo por padrao
+        o no nao sendo null retornara -1
+        leftHeight, analisa a altura da esquerda
+        rightHeight, analisa a altura da direita
+        retorna a variavel de maior valor + 1 que é uma contagem do outro no  #comentado
+    */
     heightVisitor(node) {
         if (!node)
             return -1
@@ -148,10 +213,18 @@ class BinaryTree {
     }
 
     // informa quantos nós existem na arvore
+       
+    //retorna o tamamho a partir da raiz
     size() {
+
         return this.sizeVisitor(this.root)
     }
-
+    /* 
+    se nao existir no  
+    retorna 0
+    se for o contrario
+    retorna o tamanho do no da esquerda, o tamanho do no da direita e o resultado somando 1   #comentado
+    */
     sizeVisitor(node) {
         if (!node)
             return 0
